653e1cff2ec618ebe359ca08f375bdf59da40af8
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2021-07-03 14:07:44 +0300

    created test gov.il

--- src/test/java/com/Daniel/FirstTest.java
+++ src/test/java/com/Daniel/FirstTest.java
@@ -10,29 +10,37 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstTest {
     WebDriver wd;
 
    @BeforeMethod
     public void setUp(){
         wd = new ChromeDriver();
-        wd.get("https://www.google.com/");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wd.get("https://www.gov.il/");
     }
 
     @Test
-    public void openGoogleTest(){
+    public void openGovilTest(){
         System.out.println("site opened");
 
-        wd.findElement(By.name("q")).click();
-        wd.findElement(By.name("q")).sendKeys("selenium" + Keys.ENTER);
+//        wd.findElement(By.name("q")).click();
+//        wd.findElement(By.name("q")).sendKeys("selenium" + Keys.ENTER);
+//
+//        String text = wd.findElement(By.tagName("h3")).getText();
+//
+//        Assert.assertEquals(text.toLowerCase(), "selenium");
 
-        String text = wd.findElement(By.tagName("h3")).getText();
-
-        Assert.assertEquals(text.toLowerCase(), "selenium");
+        wd.findElement(By.cssSelector("#tm-4")).click();
+        String title = wd.findElement(By.cssSelector("h1 span")).getText();
+        System.out.println(title);
 
     }
 
-    @AfterMethod
+    @AfterMethod (enabled = false)
     public void tearDown(){
        wd.quit();
     }
